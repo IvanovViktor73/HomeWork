19e191b5f0ffb435f97ae8ac1ba04504e1dd7266
viktar <v-a-i@mail.ru>
2022-11-04 18:35:00 +0100

    Initial commit

---
+++ src/HomeWork.java
@@ -0,0 +1,18 @@
+public class HomeWork {
+    public static void main(String[] args) {
+        int cookedAsterix= 3;
+        int cookedObeliks = 3 / 2;
+        boolean cookedCorrectlyAsterix = (cookedAsterix >= 5);
+        boolean cookedCorrectlyObelix = (cookedObeliks >= 5);
+        System.out.println(cookedCorrectlyAsterix);
+        System.out.println(cookedCorrectlyObelix);
+
+
+        int number = 8;
+        int anotheNumber = 7;
+        boolean evenNumber = (number % 2 == 0);
+        boolean evenNumber1 = (anotheNumber % 2 == 0);
+        System.out.println(evenNumber);
+        System.out.println(evenNumber1);
+    }
+}
